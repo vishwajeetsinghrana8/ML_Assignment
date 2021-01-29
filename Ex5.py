ddf69b296ee8bb027e8dae717754f7e62e78132d
Knowledge Shelf <vishwajeetsinghrana8@gmail.com>
2021-01-29 20:37:47 +0530

    Add files via upload

---
+++ Ex5.py
@@ -0,0 +1,7 @@
+message = 'It was a bright cold day in April, ' \
+          'and the clocks were striking thirteen.'
+
+{'I': 1, 't': 6, ' ': 13, 'w': 2, 'a': 4, 's': 3, 'b': 1, 'r': 5,
+ 'i': 6, 'g': 2, 'h': 3,
+ 'c': 3, 'o': 2, 'l': 3, 'd': 3, 'y': 1, 'n': 4,
+ 'A': 1, 'p': 1, ',': 1, 'e': 5, 'k': 2, '.': 1}
